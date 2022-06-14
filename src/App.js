b225260b4d04c0b9b482ca297e25fc258c37beae
Hongchao Liu <lbs.lhc@gmail.com>
2022-06-14 09:05:51 +0200

    Reproduce the wallet connect deps issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import WalletConnectClient from '@walletconnect/client'
 
 function App() {
+  async function walletConnectCallback() {
+    const walletConnect = await WalletConnectClient.init({
+      projectId: '6e2562e43678dd68a9070a62b6d52207',
+      relayUrl: 'wss://relay.walletconnect.com',
+      metadata: {
+        name: 'Alphium NFT',
+        description: 'Alpephium NFT Marketplace',
+        url: 'https://walletconnect.com/',
+        icons: ['https://walletconnect.com/walletconnect-logo.png']
+      }
+    })
+
+    console.log(walletConnect)
+  }
   return (
     <div className="App">
       <header className="App-header">
